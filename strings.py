3987debd58ebf7e363336cec111549cb0646c0dd
hoangmycao <41773868+hoangmycao@users.noreply.github.com>
2018-07-29 07:46:03 +1000

    Delete strings.py

--- strings.py
+++
@@ -1,7 +0,0 @@
-strings = ['This', 'list', 'is', 'now', 'all', 'together']
-sentence = ''
-for i in range(len(strings)):
-    sentence=sentence+strings[i]+" "
-    i=+1
-print(sentence)
-print(' '.join(strings))
