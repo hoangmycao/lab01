4da6f3930d9be78da21dd4372d8674546f4f420d
May <z5171419@student.unsw.edu.au>
2018-07-29 08:18:40 +1000

    Connected all elements of the original list by a space. Then, check the result of the file by JOIN

---
+++ strings.py
@@ -0,0 +1,10 @@
+strings = ['This', 'list', 'is', 'now', 'all', 'together']
+sentence = ''
+for i in range(len(strings)):
+    if (i!=(len(strings))):
+        sentence=sentence+strings[i]+" "
+    else:
+        sentence=sentence+strings[i]
+    i+=1
+print(sentence)
+print(' '.join(strings))
